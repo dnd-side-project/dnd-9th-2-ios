c0439f3ebfa03be8103d8f65464aea5cbd9426df
never-better <youtaktak@gmail.com>
2023-07-17 08:52:37 +0900

    Feat #1 - XCode 프로젝트 생성
    - SwiftUI

---
+++ promiseDemo/promiseDemo/promiseDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  promiseDemoApp.swift
+//  promiseDemo
+//
+//  Created by youtak on 2023/07/17.
+//
+
+import SwiftUI
+
+@main
+struct promiseDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
