97d37e63346cdc382813f45a474c6c8f70df8cee
yangsubinn <cindy010670@gmail.com>
2023-08-04 18:25:11 +0900

    Feat #69 - SegmentedPickerView 구현
    - SegmentedTagView, SegmentedPickerView 구현
    - View+Modifier cornerRadius 함수 추가

---
+++ Baggle/Baggle/Features/Tab/Home/Common/SegmentedPickerView.swift
@@ -0,0 +1,53 @@
+//
+//  SegmentedPickerView.swift
+//  Baggle
+//
+//  Created by 양수빈 on 2023/08/04.
+//
+
+import SwiftUI
+
+struct Segment: Identifiable {
+    var id: MeetingStatus
+    let count: Int
+    let isSelected: Bool
+    let action: () -> Void
+}
+
+struct SegmentedPickerView: View {
+    let segment: [Segment]
+
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(segment) { info in
+                SegmentedTagView(
+                    title: info.id.title,
+                    count: info.count,
+                    isSelected: info.isSelected
+                )
+                .onTapGesture {
+                    info.action()
+                }
+            }
+
+            Spacer()
+        }
+    }
+}
+
+struct SegmentedPickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        SegmentedPickerView(segment: [
+            Segment(
+                id: .ongoing,
+                count: 10,
+                isSelected: true,
+                action: { print("예정된 약속") }),
+            Segment(
+                id: .complete,
+                count: 1,
+                isSelected: false,
+                action: { print("지난 약속") })
+        ])
+    }
+}
---
+++ Baggle/Baggle/Features/Tab/Home/Common/SegmentedTagView.swift
@@ -0,0 +1,38 @@
+//
+//  SegmentedTagView.swift
+//  Baggle
+//
+//  Created by 양수빈 on 2023/08/04.
+//
+
+import SwiftUI
+
+struct SegmentedTagView: View {
+    let title: String
+    let count: Int
+    let isSelected: Bool
+
+    var body: some View {
+        HStack(spacing: 6) {
+            Text(title)
+                .font(.system(size: 16))
+                .fontWeight(.bold)
+
+            CircleNumberView(number: "\(count)", isSelected: true)
+        }
+        .padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 16))
+        .background(Color.white)
+        .opacity(isSelected ? 1 : 0.5)
+        .cornerRadius(12, corners: [.topLeft, .topRight])
+    }
+}
+
+struct SegmentedTagView_Previews: PreviewProvider {
+    static var previews: some View {
+        SegmentedTagView(title: "예정된 약속", count: 10, isSelected: true)
+            .previewLayout(.sizeThatFits)
+
+        SegmentedTagView(title: "지난 약속", count: 10, isSelected: false)
+            .previewLayout(.sizeThatFits)
+    }
+}
