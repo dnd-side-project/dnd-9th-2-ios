bc079d106f04b4cf393f8bafba0d518816c5f925
yangsubinn <cindy010670@gmail.com>
2023-08-07 02:00:49 +0900

    Feat #80 - 모임 상세 뷰 header, NaviBarView 구현

--- Baggle/Baggle/Features/MeetingDetail/MeetingDetailView.swift
+++ Baggle/Baggle/Features/MeetingDetail/MeetingDetailView.swift
@@ -15,53 +15,51 @@ struct MeetingDetailView: View {
 
     @Environment(\.dismiss) private var dismiss
 
+    // 임시 액션시트
+    @State var isActionSheetShow: Bool = false
+
     var body: some View {
 
         WithViewStore(self.store, observe: { $0 }) { viewStore in
-            ZStack {
-                VStack(spacing: 20) {
-                    Text("모임 생성")
-                        .font(.title)
+            ZStack(alignment: .top) {
+                // navibar
+                NaviBarView(naviType: .more) {
+                    viewStore.send(.backButtonTapped)
+                } rightButtonAction: {
+                    isActionSheetShow = true
+                }
+                .background(.blue.opacity(0.2))
 
-                    Text("id: \(viewStore.meetingId)")
+                ScrollView {
+
+                    // header
+                    headerView(viewStore: viewStore)
+                        .padding(.top, 56)
 
                     if let data = viewStore.meetingData {
                         Text("모임명: \(data.name), 모임 id: \(data.id)")
                     }
-
-                    HStack(spacing: 10) {
-                        Button("방 폭파하기") {
-                            viewStore.send(.deleteButtonTapped)
-                        }
-
-                        Button("방장 넘기기") {
-                            viewStore.send(.leaveButtonTapped)
-                        }
-                    }
-
-                    Button {
-                        viewStore.send(.cameraButtonTapped)
-                    } label: {
-                        Text("카메라")
-                    }
-                    .buttonStyle(BagglePrimaryStyle(size: .small))
-
-                    Button("뒤로가기") {
-                        viewStore.send(.backButtonTapped)
-                    }
-                    .buttonStyle(BagglePrimaryStyle(size: .small, shape: .round))
                 }
 
-                BaggleAlert(
-                    isPresented: Binding(
-                        get: { viewStore.isAlertPresented },
-                        set: { _ in viewStore.send(.presentAlert) }),
-                    title: viewStore.alertTitle,
-                    description: viewStore.alertDescription,
-                    rightButtonTitle: viewStore.alertRightButtonTitle) {
-                        viewStore.send(.deleteMeeting)
-                    }
+                // alert
+                baggleAlert(viewStore: viewStore)
             }
+            // 임시 액션시트
+            .confirmationDialog("임시 액션시트", isPresented: $isActionSheetShow, actions: {
+                Button("방 폭파하기") {
+                    viewStore.send(.deleteButtonTapped)
+                }
+
+                Button("방장 넘기기") {
+                    viewStore.send(.leaveButtonTapped)
+                }
+
+                Button {
+                    viewStore.send(.cameraButtonTapped)
+                } label: {
+                    Text("카메라")
+                }
+            })
             .sheet(
                 store: self.store.scope(
                     state: \.$selectOwner,
@@ -85,6 +83,67 @@ struct MeetingDetailView: View {
                 dismiss()
             })
         }
+    }
+}
+
+extension MeetingDetailView {
+    typealias Viewstore = ViewStore<MeetingDetailFeature.State, MeetingDetailFeature.Action>
+
+    func baggleAlert(viewStore: Viewstore) -> some View {
+        BaggleAlert(
+            isPresented: Binding(
+                get: { viewStore.isAlertPresented },
+                set: { _ in viewStore.send(.presentAlert) }),
+            title: viewStore.alertTitle,
+            description: viewStore.alertDescription,
+            rightButtonTitle: viewStore.alertRightButtonTitle) {
+                viewStore.send(.deleteMeeting)
+            }
+    }
+
+    func headerView(viewStore: Viewstore) -> some View {
+        VStack(alignment: .leading, spacing: 4) {
+            // 모임방 이름, 스탬프
+            HStack(alignment: .top) {
+                Text("📌")
+
+                Text("수빈님네 집들이집들이 집들이집들이집")
+                    .kerning(-0.5)
+                    .lineSpacing(8)
+                    .frame(maxWidth: 190)
+
+                Image(systemName: "stamp")
+                    .frame(width: 56, height: 23)
+                    .background(.gray)
+
+                Spacer()
+            }
+            .padding(.bottom, 10)
+            .font(.system(size: 22, weight: .bold))
+
+            // 장소, 시간
+            Text(attributedColorString(str: "장소  |  수빈님 없는 수빈님네 집",
+                                       targetStr: "장소  |",
+                                       color: .black,
+                                       targetColor: .gray))
+            .font(.system(size: 15))
+
+            Text(attributedColorString(str: "시간  |  2023년 12월 30일 18:30",
+                                       targetStr: "시간  |",
+                                       color: .black,
+                                       targetColor: .gray))
+            .font(.system(size: 15))
+            .padding(.bottom, 20)
+
+            // 메모
+            Text("작성된 메모가 있어요 작성된 메모가 있어요작성된 작성된 메모가 있어요작성된 메모가 있어요")
+                .padding(.vertical, 14)
+                .padding(.horizontal, 20)
+                .background(.white)
+                .cornerRadius(8)
+        }
+        .padding(EdgeInsets(top: 8, leading: 20, bottom: 24, trailing: 20))
+        .background(.blue.opacity(0.2))
     }
 }
 
