bcd5d1037449ddffe4e8d2c1e7233a110f4ba707
Subin Yang <cindy010670@gmail.com>
2023-08-09 13:08:42 +0900

    Feat #96 - 이미지 상세 애니메이션 추가

--- Baggle/Baggle/Features/MeetingDetail/ImageDetail/ImageDetailView.swift
+++ Baggle/Baggle/Features/MeetingDetail/ImageDetail/ImageDetailView.swift
@@ -13,6 +13,8 @@ import Kingfisher
 struct ImageDetailView: View {
 
     @Binding var isPresented: Bool
+    @State var isAnimating: Bool = false
+
     let imageURL: String
     let closeButtonAction: () -> Void
 
@@ -20,26 +22,26 @@ struct ImageDetailView: View {
         ZStack {
             ShadeView(isPresented: $isPresented)
 
-            if isPresented {
-                VStack(spacing: 16) {
-                    KFImage(URL(string: imageURL))
-                        .placeholder { _ in
-                            Color.grayF5
-                        }
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: screenSize.width - 40, height: screenSize.width - 40)
-                        .cornerRadius(12)
-                        .padding(.top, 70) // 이미지 기준 가운데 정렬 맞추기 위해 버튼 크기만큼 상단 패딩 추가
-
-                    Button("닫기") {
-                        closeButtonAction()
+            VStack(spacing: 16) {
+                KFImage(URL(string: imageURL))
+                    .placeholder { _ in
+                        Color.grayF5
                     }
-                    .buttonStyle(BaggleTertiaryStyle())
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: screenSize.width - 40, height: screenSize.width - 40)
+                    .cornerRadius(12)
+                    .padding(.top, 70) // 이미지 기준 가운데 정렬 맞추기 위해 버튼 크기만큼 상단 패딩 추가
+
+                Button("닫기") {
+                    closeButtonAction()
                 }
+                .buttonStyle(BaggleTertiaryStyle())
             }
+            .opacity(isAnimating ? 1 : 0)
+            .animation(.easeInOut(duration: 0.2), value: isAnimating)
         }
-//        .onAppear { isPresented = true }
-//        .onDisappear { isPresented = false }
+        .onAppear { isAnimating = true }
+        .onDisappear { isAnimating = false }
     }
 }
