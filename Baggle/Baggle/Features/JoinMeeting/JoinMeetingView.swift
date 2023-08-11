867bd7480c91d6c121d1a96d38f54adda1c52cf9
yangsubinn <cindy010670@gmail.com>
2023-08-11 17:14:33 +0900

    Feat #119 - 모임 참여 뷰 디자인 ui 반영

--- Baggle/Baggle/Features/JoinMeeting/JoinMeetingView.swift
+++ Baggle/Baggle/Features/JoinMeeting/JoinMeetingView.swift
@@ -16,19 +16,103 @@ struct JoinMeetingView: View {
     var body: some View {
 
         WithViewStore(self.store, observe: { $0 }) { viewStore in
-            VStack(spacing: 20) {
-                Text("모임 참여")
+            VStack(alignment: .center, spacing: 0) {
+                createDescription()
+                    .padding(.top, 46)
+                
+                createMeetingInfo()
+                    .padding(.top, 26)
+                
+                Spacer()
+                
+                Image.Illustration.invitationReceive
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.leading, 79)
+                    .padding(.trailing, 56)
+                
+                Spacer()
+                
+                createButton(viewStore: viewStore)
+                    .padding(.bottom, 16)
+            }
+        }
+    }
+}
 
-                Text("id: \(viewStore.meetingId)")
-
-                Button("모임 참여하기") {
-                    viewStore.send(.joinButtonTapped)
+extension JoinMeetingView {
+    func createDescription() -> some View {
+        VStack {
+            Text("약속 초대장이 도착했어요!\n참여하시겠어요?")
+                .multilineTextAlignment(.center)
+                .font(.system(size: 22, weight: .bold))
+                .foregroundColor(.primaryNormal)
+                .padding(.vertical, 8)
+            
+            Text("초대장을 확인하고 약속에 참여해보세요.")
+                .font(.system(size: 16, weight: .medium))
+                .foregroundColor(.gray8C)
+        }
+        .padding(.top, 8)
+    }
+    
+    func createMeetingInfo() -> some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 12) {
+                HStack(spacing: 6) {
+                    Text("📌")
+                    
+                    Text("수빈님네 집들이")
                 }
-                .buttonStyle(BagglePrimaryStyle())
-
-                Button("나중에 참여하기") {
-                    viewStore.send(.exitButtonTapped)
+                
+                VStack(alignment: .leading, spacing: 6) {
+                    Text(
+                        attributedColorString(
+                            str: "장소  |  유탁님 없는 잠실",
+                            targetStr: "장소  |",
+                            color: .black,
+                            targetColor: .gray8C
+                        )
+                    )
+                    .font(.system(size: 14))
+                    
+                    Text(
+                        attributedColorString(
+                            str: "시간  |  2023년 10월 23일 15:30",
+                            targetStr: "시간  |",
+                            color: .black,
+                            targetColor: .gray8C)
+                    )
+                    .font(.system(size: 14))
                 }
+            }
+            
+            Spacer()
+        }
+        .padding(20)
+        .frame(width: screenSize.width - 80)
+        .overlay {
+            RoundedRectangle(cornerRadius: 12)
+                .stroke(Color.primaryNormal, lineWidth: 1)
+        }
+    }
+    
+    func createButton(
+        viewStore: ViewStore<JoinMeetingFeature.State, JoinMeetingFeature.Action>
+    ) -> some View {
+        VStack(spacing: 0) {
+            Button("약속 참여하기") {
+                viewStore.send(.joinButtonTapped)
+            }
+            .buttonStyle(BagglePrimaryStyle())
+            
+            Button {
+                viewStore.send(.exitButtonTapped)
+            } label: {
+                Text("닫기")
+                    .font(.system(size: 15, weight: .medium))
+                    .foregroundColor(.grayBF)
+                    .frame(width: screenSize.width-40, height: 54)
             }
         }
     }
