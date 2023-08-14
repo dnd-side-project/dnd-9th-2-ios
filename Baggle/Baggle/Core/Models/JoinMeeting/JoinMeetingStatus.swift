b5d554921c771fafbe7aab9751f440918c1293dd
yangsubinn <cindy010670@gmail.com>
2023-08-14 16:43:31 +0900

    Feat #136 - JoinMeetingStatus 파일 분리, JoinMeetingEntity 추가

---
+++ Baggle/Baggle/Core/Models/JoinMeeting/JoinMeetingStatus.swift
@@ -0,0 +1,20 @@
+//
+//  JoinMeetingStatus.swift
+//  Baggle
+//
+//  Created by 양수빈 on 2023/08/14.
+//
+
+import Foundation
+
+enum JoinMeetingStatus: Equatable {
+    case enable(JoinMeeting)
+    case expired
+    case joined
+    case fail(APIError)
+}
+
+enum JoinMeetingResultStatus: Equatable {
+    case success
+    case fail(APIError)
+}
