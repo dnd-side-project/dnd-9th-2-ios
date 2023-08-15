2be0a3de1c475793b2b0fb39c23f9716988ef43d
Subin Yang <cindy010670@gmail.com>
2023-08-16 00:52:44 +0900

    Feat #140 - EmergencyEntity, RequestModel 생성

---
+++ Baggle/Baggle/Core/Models/Network/Emergency/EmergencyRequestModel.swift
@@ -0,0 +1,13 @@
+//
+//  EmergencyRequestModel.swift
+//  Baggle
+//
+//  Created by 양수빈 on 2023/08/16.
+//
+
+import Foundation
+
+struct EmergencyRequestModel: Equatable {
+    let meetingID: Int
+    let authorizationTime: Date
+}
