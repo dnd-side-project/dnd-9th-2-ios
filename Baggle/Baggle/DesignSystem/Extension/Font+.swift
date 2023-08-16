58ea2b5fde864e824f1390792a963552f69b1ce8
Subin Yang <cindy010670@gmail.com>
2023-08-16 14:01:06 +0900

    Fix #160 - 카카오 로그인 버튼 수정 - size, weight 입력받는 baggleFont 추가

--- Baggle/Baggle/DesignSystem/Extension/Font+.swift
+++ Baggle/Baggle/DesignSystem/Extension/Font+.swift
@@ -75,6 +75,17 @@ extension Font {
         default: return .custom("AppleSDGothicNeo-Regular", size: size)
         }
     }
+    
+    static func baggleFont(size: CGFloat, weight: Font.Weight) -> Font {
+        
+        switch weight {
+        case .bold: return .custom("AppleSDGothicNeo-Bold", size: size)
+        case .semibold: return .custom("AppleSDGothicNeo-SemiBold", size: size)
+        case .medium: return .custom("AppleSDGothicNeo-Medium", size: size)
+        case .regular: return .custom("AppleSDGothicNeo-Regular", size: size)
+        default: return .custom("AppleSDGothicNeo-Regular", size: size)
+        }
+    }
 }
 
 extension Font {
