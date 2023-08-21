f0d6c891be7bb709a478523d642f306b6e91afbf
never-better <youtaktak@gmail.com>
2023-08-21 18:01:56 +0900

    Feat #191 - Meeting Detail Entity Status 추가

--- Baggle/Baggle/Core/Models/Network/MeetingDetail/MeetingDetailEntity.swift
+++ Baggle/Baggle/Core/Models/Network/MeetingDetail/MeetingDetailEntity.swift
@@ -13,12 +13,13 @@ struct MeetingDetailEntity: Codable {
     let place: String
     let meetingTime: Date
     let memo: String?
+    let status: String
     let certificationTime: Date?
     let members: [MeetingDetailMemberEntity]
 
     enum CodingKeys: String, CodingKey {
         case meetingID = "meetingId"
-        case title, place, meetingTime, memo, certificationTime, members
+        case title, place, meetingTime, memo, status, certificationTime, members
     }
 }
 
@@ -33,7 +34,7 @@ extension MeetingDetailEntity {
             memo: self.memo,
             members: self.members.map { $0.memberDomain(meetingConfirmed: isMeetingConfirmed()) },
             memberId: memberId(username: username, members: members),
-            status: meetingStatus(date: self.meetingTime),
+            status: meetingStatus(),
             isEmergencyAuthority: isEmergencyAuthority(username: username, members: self.members),
             emergencyButtonActive: self.certificationTime != nil,
             emergencyButtonActiveTime: self.certificationTime,
@@ -43,6 +44,16 @@ extension MeetingDetailEntity {
         )
     }
 
+    private func meetingStatus() -> MeetingStatus {
+        switch status {
+        case "SCHEDULED": return .ready
+        case "ONGOING": return .progress
+        case "CONFIRMATION": return .confirmed
+        case "TERMINATION": return .completed
+        default: return meetingStatus(date: self.meetingTime)
+        }
+    }
+    
     private func meetingStatus(date: Date) -> MeetingStatus {
         
         // 약속 전날
