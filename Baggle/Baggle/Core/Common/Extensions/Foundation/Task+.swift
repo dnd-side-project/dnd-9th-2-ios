0d0721b991a112461a4d7e437c110c249692a886
yangsubinn <cindy010670@gmail.com>
2023-09-05 14:28:33 +0900

    Feat #222 - Task+ retrying 함수 추가

--- Baggle/Baggle/Core/Common/Extensions/Foundation/Task+.swift
+++ Baggle/Baggle/Core/Common/Extensions/Foundation/Task+.swift
@@ -13,3 +13,30 @@ extension Task where Success == Never, Failure == Never {
         try await Task.sleep(nanoseconds: duration)
     }
 }
+
+extension Task where Failure == Error {
+    @discardableResult
+    static func retrying(
+        priority: TaskPriority? = nil,
+        maxRetryCount: Int = 1,
+        retryDelay: TimeInterval = 0.4,
+        operation: @Sendable @escaping () async throws -> Success
+    ) -> Task {
+        Task(priority: priority) {
+            for _ in 0..<maxRetryCount {
+                do {
+                    return try await operation()
+                } catch {
+                    print("✅ retrying")
+                    let oneSecond = TimeInterval(1_000_000_000)
+                    let delay = UInt64(oneSecond * retryDelay)
+                    try await Task<Never, Never>.sleep(nanoseconds: delay)
+                    continue
+                }
+            }
+            
+            try Task<Never, Never>.checkCancellation()
+            return try await operation()
+        }
+    }
+}
